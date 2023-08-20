c6378be25d0f034acb8066002a553827b0d672c8
Samir Talwar <samir@functional.computer>
2023-08-20 16:37:36 +0200

    Flatten the daemon code a little.

--- src/daemon.rs
+++ src/daemon.rs
@@ -23,51 +23,10 @@ impl Daemon {
         listener
             .set_nonblocking(true)
             .context("Could not configure the daemon socket")?;
-        let (stop_sender, stop_receiver) = mpsc::channel::<UnixStream>();
         let running_services = RunningServices::new();
         let running_services_ = running_services.clone();
         thread::spawn(move || {
-            for stream in listener.incoming() {
-                match stream {
-                    Ok(stream) => {
-                        let stop_sender_ = stop_sender.clone();
-                        let running_services__ = running_services_.clone();
-                        thread::spawn(|| {
-                            stream.set_nonblocking(false).unwrap_or_else(|err| {
-                                eprintln!("Could not configure the server socket: {}", err)
-                            });
-                            Self::handle_connection(stream, running_services__, stop_sender_)
-                                .unwrap_or_else(|err| eprintln!("Request error: {}", err));
-                        });
-                    }
-                    Err(err) => match err.kind() {
-                        io::ErrorKind::WouldBlock => {
-                            match stop_receiver.try_recv() {
-                                Ok(mut stream) => {
-                                    bincode::serialize_into(&mut stream, &Response::Success)
-                                        .unwrap_or_else(|err| {
-                                            eprintln!(
-                                                "Failed to serialize the shutdown response: {}",
-                                                err
-                                            )
-                                        });
-                                    break;
-                                }
-                                Err(mpsc::TryRecvError::Empty) => {}
-                                Err(mpsc::TryRecvError::Disconnected) => {
-                                    eprintln!("The stop signal has been disconnected. Aborting.");
-                                    break;
-                                }
-                            }
-                            thread::sleep(Duration::from_millis(100));
-                        }
-                        _ => {
-                            eprintln!("Connection failed: {}", err);
-                            break;
-                        }
-                    },
-                }
-            }
+            start(listener, running_services_);
         });
         Ok(Self {
             socket_path,
@@ -77,33 +36,6 @@ impl Daemon {
 
     pub fn socket(&self) -> &Path {
         &self.socket_path
-    }
-
-    fn handle_connection(
-        mut stream: UnixStream,
-        running_services: RunningServices,
-        stop_sender: mpsc::Sender<UnixStream>,
-    ) -> anyhow::Result<()> {
-        let request =
-            bincode::deserialize_from(&mut stream).context("Failed to deserialize the request")?;
-        match request {
-            Request::Shutdown => stop_sender
-                .send(stream)
-                .context("Failed to shut down the daemon"),
-            Request::Start { service, wait } => match service.start() {
-                Ok(running_service) => {
-                    running_services.add(running_service);
-                    wait.block_until_ready()?;
-                    bincode::serialize_into(&mut stream, &Response::Success)
-                        .context("Failed to serialize the response")
-                }
-                Err(err) => {
-                    eprintln!("Failed to start a program: {}", err);
-                    bincode::serialize_into(&mut stream, &Response::Failure(err.to_string()))
-                        .context("Failed to serialize the response")
-                }
-            },
-        }
     }
 
     fn stop(&mut self) -> anyhow::Result<()> {
@@ -125,6 +57,80 @@ impl Drop for Daemon {
         self.running_services
             .shutdown()
             .unwrap_or_else(|err| eprintln!("An error occurred during shutdown: {}", err));
+    }
+}
+
+fn start(listener: UnixListener, running_services: RunningServices) {
+    let (stop_sender, stop_receiver) = mpsc::channel::<UnixStream>();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let stop_sender_ = stop_sender.clone();
+                let running_services_ = running_services.clone();
+                thread::spawn(|| {
+                    stream.set_nonblocking(false).unwrap_or_else(|err| {
+                        eprintln!("Could not configure the server socket: {}", err)
+                    });
+                    handle_connection(stream, running_services_, stop_sender_)
+                        .unwrap_or_else(|err| eprintln!("Request error: {}", err));
+                });
+            }
+            Err(err) => match err.kind() {
+                io::ErrorKind::WouldBlock => {
+                    if stop_requested(&stop_receiver) {
+                        break;
+                    }
+                    thread::sleep(Duration::from_millis(100));
+                }
+                _ => {
+                    eprintln!("Connection failed: {}", err);
+                    break;
+                }
+            },
+        }
+    }
+}
+
+fn handle_connection(
+    mut stream: UnixStream,
+    running_services: RunningServices,
+    stop_sender: mpsc::Sender<UnixStream>,
+) -> anyhow::Result<()> {
+    let request =
+        bincode::deserialize_from(&mut stream).context("Failed to deserialize the request")?;
+    match request {
+        Request::Shutdown => stop_sender
+            .send(stream)
+            .context("Failed to shut down the daemon"),
+        Request::Start { service, wait } => match service.start() {
+            Ok(running_service) => {
+                running_services.add(running_service);
+                wait.block_until_ready()?;
+                bincode::serialize_into(&mut stream, &Response::Success)
+                    .context("Failed to serialize the response")
+            }
+            Err(err) => {
+                eprintln!("Failed to start a program: {}", err);
+                bincode::serialize_into(&mut stream, &Response::Failure(err.to_string()))
+                    .context("Failed to serialize the response")
+            }
+        },
+    }
+}
+
+fn stop_requested(stop_receiver: &mpsc::Receiver<UnixStream>) -> bool {
+    match stop_receiver.try_recv() {
+        Ok(mut stream) => {
+            bincode::serialize_into(&mut stream, &Response::Success).unwrap_or_else(|err| {
+                eprintln!("Failed to serialize the shutdown response: {}", err)
+            });
+            true
+        }
+        Err(mpsc::TryRecvError::Empty) => false,
+        Err(mpsc::TryRecvError::Disconnected) => {
+            eprintln!("The stop signal has been disconnected. Aborting.");
+            true
+        }
     }
 }
 
