35047ac55c05328c20b8d6b031a6b65d81d90c33
Samir Talwar <samir@functional.computer>
2023-11-29 08:41:12 +0000

    Use `thiserror`.

--- src/error.rs
+++ src/error.rs
@@ -1,109 +1,63 @@
-use std::error::Error;
-use std::fmt::Display;
+use thiserror::Error;
 
 use crate::log::LoggableIoError;
 use crate::names::Name;
 
 pub type ClientResult<A> = std::result::Result<A, ClientError>;
 
-#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Error, serde::Serialize, serde::Deserialize)]
 #[serde(tag = "code", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum ClientError {
+    #[error("socket connection error: {0}")]
     SocketConnectionError(LoggableIoError),
+    #[error("{0}")]
     CommunicationError(CommunicationError),
+    #[error("daemon error: {0}")]
     DaemonError(DaemonError),
 }
 
-impl Display for ClientError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SocketConnectionError(inner) => write!(f, "socket connection error: {}", inner),
-            Self::CommunicationError(inner) => write!(f, "{}", inner),
-            Self::DaemonError(inner) => {
-                write!(f, "daemon error: {}", inner)
-            }
-        }
-    }
-}
-
-impl Error for ClientError {}
-
 pub type DaemonResult<A> = std::result::Result<A, DaemonError>;
 
-#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Error, serde::Serialize, serde::Deserialize)]
 #[serde(tag = "code", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum DaemonError {
+    #[error("socket creation error: {0}")]
     SocketCreationError(LoggableIoError),
+    #[error("socket configuration error: {0}")]
     SocketConfigurationError(LoggableIoError),
+    #[error("{0}")]
     CommunicationError(CommunicationError),
+    #[error("shutdown request error")]
     ShutdownRequestError,
-    NoSuchServiceError {
-        name: Name,
-    },
-    ServiceAlreadyExistsError {
-        name: Name,
-    },
+    #[error("no such service error (name: {name})")]
+    NoSuchServiceError { name: Name },
+    #[error("service already exists error (name: {name})")]
+    ServiceAlreadyExistsError { name: Name },
+    #[error("service crashed")]
     ServiceCrashedError,
+    #[error("start process error: {0}")]
     StartProcessError(LoggableIoError),
+    #[error("check process error: {0}")]
     CheckProcessError(LoggableIoError),
+    #[error("stop process error (id: {process_id}): {inner}")]
     StopProcessError {
         process_id: u32,
         #[serde(flatten)]
         inner: LoggableIoError,
     },
+    #[error("timed out")]
     TimeOut,
 }
 
-impl Display for DaemonError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SocketCreationError(inner) => write!(f, "socket creation error: {}", inner),
-            Self::SocketConfigurationError(inner) => {
-                write!(f, "socket configuration error: {}", inner)
-            }
-            Self::CommunicationError(inner) => write!(f, "{}", inner),
-            Self::ShutdownRequestError => write!(f, "shutdown request error"),
-            Self::NoSuchServiceError { name } => {
-                write!(f, "no such service error (name: {})", name)
-            }
-            Self::ServiceAlreadyExistsError { name } => {
-                write!(f, "service already exists error (name: {})", name)
-            }
-            Self::ServiceCrashedError => write!(f, "service crashed"),
-            Self::StartProcessError(inner) => write!(f, "start process error: {}", inner),
-            Self::CheckProcessError(inner) => write!(f, "check process error: {}", inner),
-            Self::StopProcessError { process_id, inner } => {
-                write!(f, "stop process error (id: {}): {}", process_id, inner)
-            }
-            Self::TimeOut => write!(f, "timed out"),
-        }
-    }
-}
-
-impl Error for DaemonError {}
-
 pub type CommunicationResult<A> = Result<A, CommunicationError>;
 
-#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Error, serde::Serialize, serde::Deserialize)]
 #[serde(tag = "code", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum CommunicationError {
+    #[error("serialization error: {message}")]
     SerializationError { message: String },
+    #[error("deserialization error: {message}")]
     DeserializationError { message: String },
+    #[error("connection terminated")]
     ConnectionTerminated,
 }
-
-impl Display for CommunicationError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SerializationError { message } => {
-                write!(f, "serialization error: {}", message)
-            }
-            Self::DeserializationError { message } => {
-                write!(f, "deserialization error: {}", message)
-            }
-            Self::ConnectionTerminated => write!(f, "connection terminated"),
-        }
-    }
-}
-
-impl Error for CommunicationError {}
